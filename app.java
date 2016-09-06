4149675c3ee66fffcfa37bf97c5751bf3964ec50
Eric Krause and Stephen Emery <stephen@>
2016-09-06 11:07:57 -0700

    Add prime number check

--- app.java
+++ app.java
@@ -16,6 +16,20 @@ public class App {
     Integer bB = 5;
     Integer cC = 8;
     treeInt(aA, bB, cC);
+
+    // two int two String
+    Integer dD = 6;
+    Integer eE = 9;
+    String goodbye = "Goodbye";
+    String world = "World";
+    twoBytwo(dD, eE, goodbye, world);
+
+    // Prime Check
+    Integer userNum = Integer.parseInt(console.readLine("What is your number?"));
+    boolean isPrime = false;
+
+
+    primeCheck(isPrime, userNum);
   }
 
   public static void twoArgs (String userString, int userInt ) {
@@ -23,6 +37,7 @@ public class App {
       System.out.println(userString);
     }
   }
+
   public static void treeInt (int aA, int bB, int cC) {
     Integer[] numBers = {aA, bB, cC};
     for (int i=0; i< numBers.length; i++) {
@@ -30,5 +45,28 @@ public class App {
     }
   }
 
+  public static void twoBytwo (int dD, int eE, String goodbye, String world) {
+    List<Object> twoBytwoList = new ArrayList<Object>();
+    twoBytwoList.add(dD);
+    twoBytwoList.add(eE);
+    twoBytwoList.add(goodbye);
+    twoBytwoList.add(world);
 
+    System.out.println("Contents: " + twoBytwoList);
+  }
+
+  public static void primeCheck (boolean isPrime, int userNum) {
+    for (int i=2; i<userNum; i++) {
+      if(userNum%i==0) {
+        isPrime = false;
+      }
+      isPrime = true;
+    }
+
+    if (isPrime) {
+      System.out.println("It's Prime");
+    } else {
+      System.out.println("It's not Prime");
+    }
+  }
 }
